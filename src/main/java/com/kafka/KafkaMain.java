ee0822274e4fa9c6f69403a71e793cb19f44e63b
Anil Rao <anil.rao@target.com>
2018-12-16 19:30:41 -0600

    first commit

---
+++ src/main/java/com/kafka/KafkaMain.java
@@ -0,0 +1,13 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMain {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMain.class, args);
+	}
+
+}
